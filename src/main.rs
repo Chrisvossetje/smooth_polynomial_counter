4dbe1c3558a6079a81f6dbfed0e64adc0d052cdc
Chris Vos <Chris@lucrasoft.nl>
2023-08-04 13:25:37 +0200

    lol temp

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,10 @@ use crate::algebraic_types::{Term, FieldExtension, generate_iso_polynomials};
 
 mod algebraic_types;
 
-const DEGREE: usize = 4;
+const DEGREE: usize = 5;
 const DPLUS2_CHOOSE_2: usize = ((DEGREE+2) * (DEGREE+1)) / 2;
 
-const MAX_FIELD_EXT: usize = 4;
+const MAX_FIELD_EXT: usize = 7;
 
 
 
@@ -31,15 +31,18 @@ fn main() {
     part_y_results.push(Term::generate_points_for_multiple(&part_y, n));
     part_z_results.push(Term::generate_points_for_multiple(&part_z, n));
   }
-
+  println!("Generated lookup stuff");
+  
   let iso_polys = generate_iso_polynomials(&normal);
-  // println!("{:?}", iso_polys);
+  println!("Generated isomorphic polynomials");
+  
+  println!("Start counting!");
   
   let mut smooth: usize = 0;
   'outer: for iso_poly in iso_polys { 
     let (poly, size) = iso_poly.deconstruct();
     for n in 1..=MAX_FIELD_EXT as usize {
-      if poly.has_singularity(&normal_results[n-1], &part_x_results[n-1], &part_y_results[n-1], &part_z_results[n-1], n as u32) {continue 'outer;}        // 823543
+      if poly.has_singularity(&normal_results[n-1], &part_x_results[n-1], &part_y_results[n-1], &part_z_results[n-1], n as u32) {continue 'outer;}       
     }
     smooth += size as usize;
   }
@@ -48,3 +51,15 @@ fn main() {
 
   println!("{:?}", now.elapsed());
 }
+
+
+// 1: 823543
+// 2: 724136
+// 3: 712880
+// 4: 693056
+// 5: 693056
+// 6: 690648
+// 7:
+// 8:
+// 9:
+// 10:
