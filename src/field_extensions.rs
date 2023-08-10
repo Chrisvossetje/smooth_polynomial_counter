19a83114fe3c874a54114576928646509a307881
Chris Vos <Chris@lucrasoft.nl>
2023-08-10 11:22:36 +0200

    f3_i works

--- src/field_extensions.rs
+++ src/field_extensions.rs
@@ -1,4 +1,4 @@
-use std::ops::{Add, Mul};
+use std::ops::{Add, Mul, AddAssign, MulAssign};
 
 use crate::MAX_FIELD_EXT;
 
@@ -7,96 +7,75 @@ use crate::MAX_FIELD_EXT;
 // Each coefficient is represented by 2 bits, so the can be at most 16
 #[allow(non_camel_case_types)]
 pub struct F3_i {
-  pub bits: u32,
+  pub element: u16,
   pub degree: u32,
 }
 
 impl F3_i {
-  pub fn new(bits: u32, degree: u32) -> F3_i {
-    F3_i { bits: bits, degree: degree }
+  pub fn new(element: u16, degree: u32) -> F3_i {
+    F3_i { element: element, degree: degree }
   }
 
   // print each coefficient as a number
   pub fn print(&self) {
     for j in 0..self.degree {
       let i = self.degree - j - 1;
-      let num = (self.bits >> (2 * i)) & 0b11;
+      let num = (self.element >> (2 * i)) & 0b11;
       if num >= 3 {
         panic!("Invalid number: {}, deg: {}", num, i);
       }
-      print!("{}", (self.bits >> (2 * i)) & 0b11);
+      print!("{}", (self.element >> (2 * i)) & 0b11);
     }
     println!(" ({})", self.degree);
   }
 
+  fn internal_add(a: u64, b: u64) -> u64 {
+    const M1: u64 = 0x5555;
+    const M2: u64 = 0xAAAA;
 
-  // add two polynomials, reducing the result
-  fn internal_add(a: u32, b: u32, degree: u32) -> u32 {
-    // loop through all coefficients
+    let xor = a^b;
+    let and = a&b;
+
+    let one = (and & M1) << 1;
+    let two = (and & M2) >> 1;
+
+    let ab = ((a&M2) >> 1) & b;
+    let ba = ((b&M2) >> 1) & a;
+
+    let mul = (ab | ba) * 0b11;
+
+    (mul ^ xor) | one | two
+  }
+
+  fn clmul(lhs: u64, rhs: u64, N: u8) -> u64 {
     let mut result = 0;
-    for i in 0..degree {
-        // get the coefficient of a and b
-        let a_coeff = (a >> (2 * i)) & 0b11;
-        let b_coeff = (b >> (2 * i)) & 0b11;
-    
-        // add them together
-        let mut result_coeff = a_coeff + b_coeff;
-    
-        // if the result is bigger than 2, subtract 3
-        if result_coeff > 2 {
-            result_coeff -= 3;
-        }
-    
-        // set the result coefficient
-        result |= result_coeff << (2 * i);
+    for i in 0..N {
+      let factor = (lhs >> (2*i)) & 0b11;
+      match factor {
+          2 => {result = F3_i::internal_add(result, rhs << 2*i); result = F3_i::internal_add(result, rhs << 2*i);}
+          1 => {result = F3_i::internal_add(result, rhs << 2*i);}
+          _ => {}
+      }
     }
     result
   }
 
   // We first convert the polynomials to vectors with coefficients in Z/3Z
   // Then we multiply them and reduce the result
-  fn internal_mul(a: u32, b: u32, degree: u32) -> u32 {
+  fn internal_mul(a: u64, b: u64, N: u8) -> u16 {
+    const IRRED_POLY: [u64; 7] = [0b0000, 0b0001, 0b0010, 0b0110,0b1001, 0b0110, 0b1001];
 
-    const IRRED_POLY: [[u16;2]; 7] = [[0,0], [1,0], [2,0], [2,1], [2,2], [2,1], [2,2]];
+    let bitmask: u64 = !((!0) << 2*N);
+    let irred = IRRED_POLY[N as usize];
 
-    // convert a and b to vectors
-    let mut a_vec: Vec<u16> = Vec::new();
-    let mut b_vec: Vec<u16> = Vec::new();
-    for i in 0..degree {
-        a_vec.push(((a >> (2 * i)) & 0b11) as u16);
-        b_vec.push(((b >> (2 * i)) & 0b11) as u16);
-    }
-    
-    // multiply them
-    let mut result_vec = vec![0; (2 * degree - 1) as usize];
-    for i in 0..degree {
-        for j in 0..degree {
-            result_vec[(i + j) as usize] += a_vec[i as usize] * b_vec[j as usize];
-        }
+    let mut result = F3_i::clmul(a, b, N); 
+    while (result >> N*2) > 0 {
+      let lsb = result & bitmask;
+      let msb = result >> 2*N;
+      result = F3_i::internal_add(lsb, F3_i::clmul(msb, irred, N));
     }
 
-    // while still has coefficients of index greater than degree
-    // use the identity x^degree = irred_poly[degree]
-
-    while (result_vec.len() as u32) > degree {
-        let mut index = result_vec.len() - 1;
-        let mut coeff = result_vec[index];
-        while coeff == 0 {
-            index -= 1;
-            coeff = result_vec[index];
-        }
-        let mut irred_poly = IRRED_POLY[index - degree as usize].clone();
-        for i in 0..irred_poly.len() {
-            irred_poly[i] = (irred_poly[i] * coeff) % 3;
-        }
-        for i in 0..irred_poly.len() {
-            result_vec[index - degree as usize + i] += irred_poly[i];
-        }
-        result_vec.truncate(index);
-    }
-    
-    todo!();
-
+    result as u16
   }
 
 }
@@ -105,8 +84,7 @@ impl Add for F3_i {
   type Output = Self;
   
   fn add(self, rhs: Self) -> Self::Output {
-    debug_assert_eq!(self.degree, rhs.degree);
-    F3_i {bits: F3_i::internal_add(self.bits, rhs.bits, self.degree), degree: self.degree}
+    F3_i {element: F3_i::internal_add(self.element as u64, rhs.element as u64) as u16, degree: self.degree}
   }
 }
 
@@ -114,7 +92,18 @@ impl Mul for F3_i {
   type Output = Self;
 
   fn mul(self, rhs: Self) -> Self::Output {
-    debug_assert_eq!(self.degree, rhs.degree);
-    F3_i {bits: F3_i::internal_mul(self.bits, rhs.bits, self.degree), degree: self.degree}
+    F3_i {element: F3_i::internal_mul(self.element as u64, rhs.element as u64, self.degree as u8), degree: self.degree}
+  }
+}
+
+impl AddAssign for F3_i {
+  fn add_assign(&mut self, rhs: Self) {
+    self.element = F3_i::internal_add(self.element as u64, rhs.element as u64) as u16;
+  }
+}
+
+impl MulAssign for F3_i {
+  fn mul_assign(&mut self, rhs: Self) {
+    self.element = F3_i::internal_mul(self.element as u64, rhs.element as u64, self.degree as u8);
   }
 }
